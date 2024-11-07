c32fd8039058c036b79ec5090e03c2f12f855830
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-07 23:24:27 +0600

    react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { APIProvider } from '@vis.gl/react-google-maps'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <APIProvider>
-      <App />
+      <RouterProvider router={router}></RouterProvider>
     </APIProvider>
   </StrictMode>,
 )
