fd9a5bbfcc161f7f35400445dc8e6b7beb83461e
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-07 23:42:11 +0600

    MainLout

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div className='h-16'>
+
+            </div>
+        </div>
+    );
+};
+
+export default MainLayout;
