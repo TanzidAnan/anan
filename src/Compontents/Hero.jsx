49d055e00a9d5eed70e191322f5b5349eaf7ac52
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-08 23:07:51 +0600

    hero section

--- src/Compontents/Hero.jsx
+++ src/Compontents/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
             <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content text-center">
     <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
+      <h1 className="text-5xl font-bold">Hello <span>there</span></h1>
       <p className="py-6">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
