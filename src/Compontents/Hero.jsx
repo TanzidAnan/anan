1892a38d0828d2560c193e9ac1f29b4472cd77f2
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-08 22:43:21 +0600

    button

--- src/Compontents/Hero.jsx
+++ src/Compontents/Hero.jsx
@@ -10,8 +10,10 @@ const Hero = () => {
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
       </p>
+      <div className="gap-5 flex">
       <button className="btn btn-primary">Get Started</button>
       <button className="btn btn-primary">Get Started</button>
+      </div>
     </div>
   </div>
 </div>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Nav from '../Compontents/Nav';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Compontents/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <div className='h-16'>
             <Nav></Nav>
+            
             </div>
+            <div>
             <Outlet></Outlet>
+            </div>
+            {/* <Footer></Footer> */}
         </div>
     );
 };
