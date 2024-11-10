ec99d1a8ee130cfbd33b35ba9a960bf91582cc5d
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-10 22:15:20 +0600

    about

--- src/Compontents/Nav.jsx
+++ src/Compontents/Nav.jsx
@@ -27,6 +27,7 @@ const Nav = () => {
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1 gap-4 mr-4">
                   <NavLink to='/' className={({isActive}) => isActive ? 'bg-red-700 p-2 rounded-md':'p-2'}><a>Home</a></NavLink>
+                  <NavLink to='/' className={({isActive}) => isActive ? 'bg-red-700 p-2 rounded-md':'p-2'}><a>About</a></NavLink>
                     <NavLink to='/blog' className={({isActive}) =>isActive? 'bg-red-700 p-2 rounded-md':'p-2'}><a>Bloge</a></NavLink>
                     <NavLink to='/bookMark' className={({isActive}) =>isActive? 'bg-red-700 p-2 rounded-md':'p-2'}><a>BookMark</a></NavLink>
                 </ul>
